68fda0b6898488dbaa8cf9374d088aadc70be368
hassan-ayinde <hassantoheebayinde@gmail.com>
2025-05-30 03:04:16 +0100

    chore: install shadcn library

--- Github_repository_finder/src/App.tsx
+++ Github_repository_finder/src/App.tsx
@@ -1,12 +1,16 @@
+import Appcontainer from './components/Appcontainer'
 import './App.css'
+import Header from './components/Header'
+import LanguageSelector from './components/LanguageSelector'
 
 function App() {
 
 
   return (
-    <>
-    
-    </>
+    <Appcontainer>
+      <Header />
+      <LanguageSelector />
+    </Appcontainer>
   )
 }
 
