615bb527a6b958f39e9fca35628e704fc7606ee2
hassan-ayinde <hassantoheebayinde@gmail.com>
2025-06-01 12:25:20 +0100

    feat: implementing state lifting concept

--- Github_repository_finder/src/App.tsx
+++ Github_repository_finder/src/App.tsx
@@ -1,15 +1,29 @@
+import {useState} from 'react'
 import Appcontainer from './components/Appcontainer'
 import './App.css'
 import Header from './components/Header'
 import LanguageSelector from './components/LanguageSelector'
+import GithubInfo from './components/GithubInfo'
+
 
 function App() {
-
+  const [selectedLanguage, setSelectedLanguage] = useState<string | null>(null)
 
   return (
     <Appcontainer>
       <Header />
-      <LanguageSelector />
+      <LanguageSelector 
+        value={selectedLanguage}
+        onValueChange={(lang: string) => setSelectedLanguage(lang)}
+      />
+
+      {!selectedLanguage && (
+        <p className="text-center mt-4 text-gray-500">
+          Please select a language
+        </p>
+      )}
+
+      {selectedLanguage && <GithubInfo selectedLanguage={selectedLanguage}/>}
     </Appcontainer>
   )
 }
