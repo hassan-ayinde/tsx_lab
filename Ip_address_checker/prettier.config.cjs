adcdbeab2e2eb3d9dd38b96944ce0db189bd0a33
hassan-ayinde <hassantoheebayinde@gmail.com>
2025-06-14 21:23:18 +0100

    chore: initial project setup with React + Vite + TypeScript

---
+++ Ip_address_checker/prettier.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 100,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+};
