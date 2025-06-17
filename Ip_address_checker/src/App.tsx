4e974af3bd84dd3df132b53060b43b00920e3579
hassan-ayinde <hassantoheebayinde@gmail.com>
2025-06-17 23:54:31 +0100

    feat: integrate map visibility toggle and context provider into App component

--- Ip_address_checker/src/App.tsx
+++ Ip_address_checker/src/App.tsx
@@ -1,7 +1,26 @@
 import './App.css';
+import AppContainer from '@/components/AppContainer';
+import Header from '@/components/Header';
+import InfoPanel from '@/components/InfoPanel';
+import {IpDataProvider} from './context/IpDataContext';
+import MyMap from '@/components/MyMap';
+import 'leaflet/dist/leaflet.css';
+import '@/lib/fixLeafletIcon';
+import { useState } from 'react';
 
 function App() {
-  return <></>;
+   const [showMap, setShowMap] = useState<boolean>(false);
+  return (
+    <>
+      <IpDataProvider>
+        <AppContainer>
+          <Header onMapShow={() => setShowMap(true)}/>
+          <InfoPanel />
+          {showMap && <MyMap />}
+        </AppContainer>
+      </IpDataProvider>
+  </>
+);
 }
 
-export default App;
+export default App;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
