a0702bb404397f403edd1bfa4165da51be7200cc
hassan-ayinde <hassantoheebayinde@gmail.com>
2025-06-17 23:46:48 +0100

    feat: add responsive header with dynamic height based on search trigger

--- Ip_address_checker/src/components/Header.tsx
+++ Ip_address_checker/src/components/Header.tsx
@@ -1,21 +1,33 @@
 import React from 'react'
 import { FaGripfire } from "react-icons/fa";
 import SearcBar from './SearcBar';
+import { useState } from 'react';
 
-const Header: React.FC = () => {
-    
+interface HeaderProps {
+  onMapShow: () => void;
+}
 
+const Header: React.FC<HeaderProps> = ({onMapShow}) => {
+    const [ipAddress, setIpAddress] = useState<string>("");
+    const [isSearchTriggered, setIsSearchTriggered] = useState<boolean>(false);
   return (
     <header 
-        className='text-center bg-[url("/src/assets/pattern-bg-desktop.png")]
-        bg-cover bg-no-repeat bg-center text-white h-[300px] py-6'>
+        className={`text-center bg-[url("/src/assets/pattern-bg-desktop.png")]
+        bg-cover bg-no-repeat bg-center text-white ${isSearchTriggered ? 'h-[50vh]' : 'h-screen'} 
+        flex flex-col items-center justify-center`}>
         <div className='flex items-center gap-1 justify-center md:text-3xl font-bold pb-2'>
-            <FaGripfire />
-            <h1>IP Address Checker</h1>
+          <FaGripfire />
+          <h1>IP Address Checker</h1>
         </div>
-        <p className='text-lg'>Enter an IP address to check its validity and details.</p>
+        <p className='text-sm sm:text-lg'>Search any IP address or domain to check its validity and details.</p>
 
-        <SearcBar/>
+        <SearcBar 
+          onSearch = {() => {
+            setIsSearchTriggered(true);
+            onMapShow();
+          }} 
+          ipAddress = {ipAddress} setIpAddress={setIpAddress}
+        />
     </header>
   )
 }
