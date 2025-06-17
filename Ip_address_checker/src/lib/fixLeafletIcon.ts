675b7b6cf0224477235c30568eb7b2a6397ff357
hassan-ayinde <hassantoheebayinde@gmail.com>
2025-06-17 23:52:56 +0100

    fix: override default Leaflet marker icons to resolve missing icon issue

---
+++ Ip_address_checker/src/lib/fixLeafletIcon.ts
@@ -0,0 +1,14 @@
+// src/utils/fixLeafletIcon.ts
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// @ts-expect-error: _getIconUrl is a private property not exposed in types, but needs to be deleted for custom icon URLs
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
