21ee6a7b18e14abe975fb4b8a325e83e5cfe7b6c
hassan-ayinde <hassantoheebayinde@gmail.com>
2025-06-19 02:10:05 +0100

    refactor: import useIpData hook from lib directory

--- Ip_address_checker/src/context/IpDataContext.tsx
+++ Ip_address_checker/src/context/IpDataContext.tsx
@@ -1,26 +1,7 @@
-// src/context/IpDataContext.tsx
-import { createContext, useContext, useState } from "react";
+import {useState } from "react";
 import type { ReactNode } from "react";
-
-interface IpData {
-  ip: string;
-  location: {
-    country: string;
-    city: string;
-    postalCode: string;
-    lat: number;
-    lng: number;
-    timezone: string;
-  };
-  isp: string;
-}
-
-interface IpDataContextType {
-  ipData: IpData | null;
-  setIpData: (data: IpData) => void;
-}
-
-const IpDataContext = createContext<IpDataContextType | undefined>(undefined);
+import type { IpData } from "./IpDataContextInstance";
+import { IpDataContext } from "./IpDataContextInstance";
 
 export const IpDataProvider = ({ children }: { children: ReactNode }) => {
   const [ipData, setIpData] = useState<IpData | null>(null);
@@ -30,12 +11,4 @@ export const IpDataProvider = ({ children }: { children: ReactNode }) => {
       {children}
     </IpDataContext.Provider>
   );
-};
-
-export const useIpData = () => {
-  const context = useContext(IpDataContext);
-  if (!context) {
-    throw new Error("useIpData must be used within an IpDataProvider");
-  }
-  return context;
 };
