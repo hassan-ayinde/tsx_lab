6ec0f4dfb1a56454ba954ad358f6efce44dd4596
hassan-ayinde <hassantoheebayinde@gmail.com>
2025-06-21 16:09:05 +0100

    chore: change indentation from 4 spaces to 2 for better code alignment

--- Ip_address_checker/src/components/Header.tsx
+++ Ip_address_checker/src/components/Header.tsx
@@ -8,28 +8,28 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({onMapShow}) => {
-    const [ipAddress, setIpAddress] = useState<string>("");
-    const [isSearchTriggered, setIsSearchTriggered] = useState<boolean>(false);
-    const [error,setError] = useState<string | null>(null)
+  const [ipAddress, setIpAddress] = useState<string>("");
+  const [isSearchTriggered, setIsSearchTriggered] = useState<boolean>(false);
+  const [error,setError] = useState<string | null>(null)
   return (
     <header 
-        className={`text-center bg-[url("/src/assets/pattern-bg-desktop.png")]
-        bg-cover bg-no-repeat bg-center text-white ${isSearchTriggered ? 'h-[50vh]' : 'h-screen'} 
-        flex flex-col items-center justify-center`}>
-        <div className='flex items-center gap-1 justify-center md:text-3xl font-bold pb-2'>
-          <FaGripfire />
-          <h1>IP Address Checker</h1>
-        </div>
-        <p className='text-sm sm:text-lg'>Search any IP address or domain to check its validity and details.</p>
-
-        <SearcBar 
-          onSearch = {() => {
-            setIsSearchTriggered(true);
-            onMapShow();
-          }} 
-          ipAddress = {ipAddress} setIpAddress={setIpAddress}
-          error={error} setError={setError}
-        />
+      className={`text-center bg-[url("/src/assets/pattern-bg-desktop.png")]
+      bg-cover bg-no-repeat bg-center text-white ${isSearchTriggered ? 'h-[50vh]' : 'h-screen'} 
+      flex flex-col items-center justify-center `}
+    >
+      <div className='flex items-center gap-1 justify-center md:text-3xl font-bold pb-2'>
+        <FaGripfire />
+        <h1>IP Address Checker</h1>
+      </div>
+      <p className='text-sm sm:text-lg'>Search any IP address or domain to check its validity and details.</p>
+      <SearcBar 
+        onSearch = {() => {
+          setIsSearchTriggered(true);
+          onMapShow();
+        }} 
+        ipAddress = {ipAddress} setIpAddress={setIpAddress}
+        error={error} setError={setError}
+      />
     </header>
   )
 }
